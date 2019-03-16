737c88b1c52544ea63d69795fcd9f4e7bdd82c1e
sendhilkumar <sendhilkumar.bala@gmail.com>
2019-03-16 15:55:20 +0530

    schema name fix

--- server/src/main/java/tracker/data/portfolio/loader/CASLoader.java
+++ server/src/main/java/tracker/data/portfolio/loader/CASLoader.java
@@ -8,10 +8,14 @@ import tracker.domain.SchemeFinder;
 import tracker.domain.Transaction;
 import tracker.domain.TransactionFinder;
 import tracker.domain.TransactionList;
+import tracker.server.SchemesServices;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class CASLoader {
     private static Logger logger = LoggerFactory.getLogger(CASLoader.class.getName());
@@ -21,10 +25,17 @@ public class CASLoader {
         SchemeFinderByName schemeFinderByName = new SchemeFinderByName(SchemeFinder.findMany(SchemeFinder.all()));
 
         List<Transaction> transactions = new Parser().parse(casPdf, password);
+        logger.info("Read "+transactions.size() + " from "+casPdf);
+
+        Map<String, Integer> schemeCodeCache = new HashMap<>();
         for (Transaction transaction : transactions) {
-            transaction.setSchemeCode(schemeFinderByName.find(transaction.getSchemeName()));
+            String schemeName = transaction.getSchemeName();
+            int schemeCode = schemeCodeCache.computeIfAbsent(schemeName, s -> schemeFinderByName.find(schemeName));
+            transaction.setSchemeCode(schemeCode);
         }
         TransactionList transactionList = new TransactionList(transactions);
+
+        logger.info("Beginning to insert transactions");
 
         MithraManager.getInstance().executeTransactionalCommand(tx -> {
             TransactionFinder.findMany(TransactionFinder.all()).deleteAll();
--- server/src/main/java/tracker/data/portfolio/loader/SchemeFinderByName.java
+++ server/src/main/java/tracker/data/portfolio/loader/SchemeFinderByName.java
@@ -2,15 +2,22 @@ package tracker.data.portfolio.loader;
 
 import com.gs.collections.impl.list.mutable.FastList;
 import org.apache.commons.text.similarity.LevenshteinDistance;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import tracker.domain.Scheme;
-import tracker.domain.SchemeFinder;
 import tracker.domain.SchemeList;
+import tracker.server.SchemesServices;
 
+import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
 
 public class SchemeFinderByName {
+    private static Logger logger = LoggerFactory.getLogger(SchemeFinderByName.class.getName());
+
     private final SchemeList schemes;
+    private final SchemesServices schemesServices = new SchemesServices();
 
     public SchemeFinderByName(SchemeList schemes) {
         this.schemes = schemes;
@@ -23,18 +30,38 @@ public class SchemeFinderByName {
         if (advisorIndex != -1) {
             schemeNameFromTx = schemeNameFromTx.substring(0, advisorIndex);
         }
-        schemeNameFromTx = schemeNameFromTx.replaceAll("motilal oswal most", "most");
+        schemeNameFromTx = schemeNameFromTx.replaceAll("motilal oswal most focused", "most focused");
+        schemeNameFromTx = schemeNameFromTx.replaceAll("motilal oswal focused", "most focused");
+        schemeNameFromTx = schemeNameFromTx.replaceAll("motilal oswal", "most focused");
         if (Character.isDigit(schemeNameFromTx.charAt(0))) {
             schemeNameFromTx = schemeNameFromTx.substring(schemeNameFromTx.indexOf(' ') + 1);
         }
         schemeNameFromTx = simplify(schemeNameFromTx);
 
-        List<Match> similarTexts = similarTexts(schemeNameFromTx, new LevenshteinDistance()::apply);
-        for (Match topMatch : similarTexts) {
-            System.out.println(topMatch.distance + " :: " + topMatch.match);
+        SimilarityScore similarityScore = new LevenshteinDistance()::apply;
+
+        List<Match> similarTexts = similarTexts(schemeNameFromTx, similarityScore);
+        Match topMatch = similarTexts.get(0);
+
+        int schemeCode;
+        try {
+            List<Scheme> schemes = schemesServices.find(schemeNameFromTx);
+            Scheme scheme = schemes.get(0);
+
+            Integer score = similarityScore.apply(schemeNameFromTx, simplify(scheme.getName().toLowerCase()));
+            if (topMatch.distance < score) {
+                schemeCode = topMatch.schemeCode;
+                logger.info(1 + "::" + topMatch.distance + " :: " + topMatch.match);
+            }else{
+                schemeCode = scheme.getCode();
+                logger.info(2 + "::" + score + " :: " + scheme.getName());
+            }
+            logger.info("Mapping::"+schemeNameFromTx + "::" + scheme.getName());
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
         }
 
-        return similarTexts.get(0).schemeCode;
+        return schemeCode;
     }
 
     private List<Match> similarTexts(String schemeNameFromTx, SimilarityScore similarityScore) {
@@ -44,16 +71,17 @@ public class SchemeFinderByName {
             if (schemeNameFromTx.equals(schemeName)) {
                 return FastList.newListWith(new Match(scheme.getCode(), schemeName, 0));
             }
-            Integer result = similarityScore.apply(schemeNameFromTx, simplify(schemeName.toLowerCase()));
-            Match match = new Match(scheme.getCode(), schemeName, result);
+            Integer distance = similarityScore.apply(schemeNameFromTx, simplify(schemeName.toLowerCase()));
+            Match match = new Match(scheme.getCode(), schemeName, distance);
             matches.add(match);
         }
         matches.sort(Comparator.comparingInt(o -> o.distance));
-        return FastList.newListWith(matches.get(0), matches.get(1));
+        return FastList.newListWith(matches.get(0), matches.get(1), matches.get(2), matches.get(3), matches.get(5));
     }
 
     private static String simplify(String schemeName) {
         schemeName = schemeName.replaceAll("-", " ");
+        schemeName = schemeName.replaceAll("/", " ");
         schemeName = schemeName.replaceAll("fund", "");
         schemeName = schemeName.replaceAll("direct plan", "direct");
         schemeName = schemeName.replaceAll("growth plan", "growth");
